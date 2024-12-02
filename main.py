8890abf8ed70e51755f025f44d16a360911754fa
Teo <fkite@tuta.io>
2024-12-02 11:36:02 +0100

    Add use of ComputerVision class in the main function

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import cv2
 from thymio import Thymio
+from ComputerVision import ComputerVision
 
 
 if __name__ == "__main__":
-    # initialize the camera
 
-
-    # get position and angle of the thymio
-
+    vision = ComputerVision(1)
 
     # create and initialize the Kalman filter
 
@@ -22,7 +20,36 @@ if __name__ == "__main__":
         if ...:
             thymio.local_navigation()
 
-        # vision
+        # DETECTION OF THE MARKER (ROBOT and GOAL)
+        ret, frame = vision.cam.read()
+        if not ret:
+            print("Failed to capture frame. Exiting.")
+            break
+
+        frame_with_markers, marker_ids, rvecs, tvecs = vision.detect_and_estimate_pose(frame)
+
+        markers_data = []
+        if marker_ids is not None:
+            frame_with_vectors, markers_data = vision.process_marker_pose(frame_with_markers, marker_ids, rvecs, tvecs)
+        else:
+            frame_with_vectors = frame.copy()
+
+        # DETECTION OF EDGES and CORNERS (red mask)
+        frame_masked = vision.apply_color_mask(frame, mask_color='r')
+        edges = vision.detect_edges(frame_masked)
+        corners = vision.get_corners_and_shape_edges(edges)
+
+        frame_aruco_and_corners = frame_with_vectors.copy()
+
+        # Visualize corners
+        for shape in corners:
+            for x, y in shape:
+                cv2.circle(frame_aruco_and_corners, (x, y), 5, (0, 255, 0), -1)
+        
+
+        # DETECTION IF CAMERA IS COVERED
+        camera_covered = vision.is_camera_covered(marker_ids)
+        print(f"Robot not detected: {camera_covered} (ID robot = {vision.ARUCO_ROBOT_ID})")
 
         # Kalman filter
 
