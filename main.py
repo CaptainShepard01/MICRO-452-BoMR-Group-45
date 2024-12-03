cbacebb769993f77da5e917617f89241a2958f83
Anton Balykov <49350081+CaptainShepard01@users.noreply.github.com>
2024-12-03 17:25:30 +0100

    Filling in the main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+import os
+
 import cv2
 import numpy as np
 
@@ -21,6 +24,8 @@ def get_frame_with_vectors(vision, frame):
     return frame_with_vectors, markers_data, marker_ids
 
 def get_thymio_localisation(markers_data):
+    thymio_pos_x, thymio_pos_y, thymio_theta = None, None, None
+
     for marker in markers_data:
         if marker[0] == vision.ARUCO_ROBOT_ID:
             thymio_pos_x, thymio_pos_y = marker[1], marker[2]
@@ -29,6 +34,8 @@ def get_thymio_localisation(markers_data):
     return thymio_pos_x, thymio_pos_y, thymio_theta
 
 def get_goal_position(markers_data):
+    goal_pos = None
+
     for marker in markers_data:
         if marker[0] == vision.ARUCO_GOAL_ID:
             goal_pos = [marker[1], marker[2]]
@@ -36,9 +43,25 @@ def get_goal_position(markers_data):
 
     return goal_pos
 
+def distance(p1, p2):
+    """Calculate the Euclidean distance between two points."""
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+def merge_close_corners_simple(corners, threshold: float = 5):
+    def merge_shape(shape):
+        unique_points = []
+        for point in shape:
+            if not any(distance(point, unique_point) < threshold for unique_point in unique_points):
+                unique_points.append(point)
+        return unique_points
+
+    return [merge_shape(shape) for shape in corners]
+
 
 if __name__ == "__main__":
     vision = ComputerVision(1)
+    thymio = Thymio()
+    thymio.reset_leds()
 
     # Detecting initial Thymio position
     ret = None
@@ -47,19 +70,20 @@ if __name__ == "__main__":
         ret, frame = vision.cam.read()
 
     frame_with_vectors, markers_data, marker_ids = get_frame_with_vectors(vision, frame)
-    thymio_pos_x, thymio_pos_y, thymio_theta = get_thymio_localisation(markers_data)
 
     frame_masked = vision.apply_color_mask(frame, mask_color='r')  # We apply a red mask to detect only red obstacles
     edges = vision.detect_edges(frame_masked)  # We detect the edges on the masked frame using Canny
     corners = vision.get_corners_and_shape_edges(edges)  # We obtain shapes and corners by approxPolyDP
+    corners = merge_close_corners_simple(corners, threshold=5)
+    corners = [corner for corner in corners if len(corner) > 2]
+
+    thymio_pos_x, thymio_pos_y, thymio_theta = get_thymio_localisation(markers_data)
     goal_pos = get_goal_position(markers_data)
 
     # Global path planning
     navigation = Navigation(corners, [thymio_pos_x, thymio_pos_y], goal_pos)
     global_path = navigation.get_shortest_path()
 
-    # create and initialize the Thymio
-    thymio = Thymio()
     thymio.set_position(np.array([thymio_pos_x, thymio_pos_y]))
     thymio.set_orientation(thymio_theta)
 
@@ -92,7 +116,12 @@ if __name__ == "__main__":
         # Visualize corners
         for shape in corners:
             for x, y in shape:
-                cv2.circle(frame_aruco_and_corners, (x, y), 5, (0, 255, 0), -1)
+                cv2.circle(frame_aruco_and_corners, (x, y), 2, (0, 255, 0), -1)
+
+        # for point in global_path:
+        #     cv2.circle(frame_aruco_and_corners, (int(point[0]), int(point[1])), 2, (0, 0, 255), -1)
+
+        cv2.imshow("Main", frame_aruco_and_corners)
         
 
         ### Detection if camera is covered ###
